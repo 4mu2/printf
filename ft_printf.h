12c657a8f7023693e62a02a03cb1dde211050e12
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-07-31 22:55:36 +0300

    42

---
+++ ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mumutlu <mumutlu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/31 18:45:42 by mumutlu           #+#    #+#             */
+/*   Updated: 2023/07/31 18:45:43 by mumutlu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(int nb);
+int	ft_putnbr_unsigned(unsigned int nb);
+int	base(unsigned long long int k, char x);
+int	ft_strlen(const char *s);
+int	ft_printf(const char *s, ...);
+
+#endif
